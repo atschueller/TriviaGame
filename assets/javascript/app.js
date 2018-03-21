b0e39b2aaf653420acb765fe4eede52081ab81a7
Adrienne Schueller <atschueller@gmail.com>
2018-03-21 02:08:26 -0400

    buttons working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,45 +3,64 @@ var startButton = $('#startButton');
 var correctScore = 0;
 var wrongScore = 0;
 var timerRunning = false;
-var time = 60;
+var time = 45;
 var intervalID;
 
 //Have JS/Jquery start after document is loaded//
 $(document).ready(function () {
+    //onClick Functions//
+    $('#startButton').on('click', function (beginGame) {
+        $('#startButton').hide();
+        $('.gameBody').show();
+        timerGoing();
+    });
+    $('#doneButton').on('click', function (finished) {
+        submitAnswers();
+    });
+    $('#reStart').on('click', function (reStart) {
+        resetGame();
+    });
+
+    //Reset Game Function//
     function resetGame() {
         $('#startButton').show();
         $('.gameBody').hide();
         $('.answerLog').hide();
         correctScore = 0;
         wrongScore = 0;
+        timerRunning = false;
+        time = 45;
     };
     resetGame();
 
-    $('#startButton').on('click', function () {
-        $('#startButton').hide();
-        $('.gameBody').show();
-        timerGoing();
-    });
-
-    //Create timer functions//
+    //Timer Functions//
     function timerGoing() {
         intervalId = setInterval(decreasingTimer, 1000);
     };
+    function stopTimer() {
+        clearInterval(intervalId);
+
+    };
+    stopTimer();
 
     function decreasingTimer() {
         time--;
         $("#timer").html("<div>" + time + "</div>");
 
         if (time === 0) {
-            clearInterval(intervalId);
+            stopTimer();
             alert('Time is Up!!');
-            resetGame();
-        };
+            $('.gameBody').hide();
+            $('.answerLog').show()
+        } else {
+            $('#doneButton').click(finished);
+        }
     };
     decreasingTimer();
 
     timerGoing();
 
+//User Answers logged and matched//
     function submitAnswers() {
         var questions = 5;
         var q1 = $('input[name=q1]:checked').val();
@@ -53,6 +72,7 @@ $(document).ready(function () {
 
         for (i = 1; i <= questions.length; i++) {
             if (eval('q' + 1) == answers[0 + 1]) {
+                console.log();
                 correctScore++;
                 $('#correctAnswers').html('Correct Answers:' + correctScore.toString());
             } else {
@@ -60,28 +80,13 @@ $(document).ready(function () {
                 $('#wrongAnswers').html('Wrong Answers:' + wrongScore.toString());
             };
         };
+        stopTimer();
+        $('.gameBody').hide();
+        $('.answerLog').show();
     };
     submitAnswers();
 
-    $("#doneButton").on('click', function () {
-        $('.gameBody').hide();
-        $('.answerLog').show();
-    });
-
-    $('#reStart').on('click', function () {
-        resetGame();
-    })
-
-
-
-
-
-
-
-
-
-
-
+    
 
 })// final end
 
