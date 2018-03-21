a0b9ef84a993d7376c7e01742a194141a9023b0d
Adrienne Schueller <atschueller@gmail.com>
2018-03-21 00:34:42 -0400

    new approach

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,87 +1,70 @@
 //Variables Needed//
 var startButton = $('#startButton');
-var questions = [];
-var correctAnswer = 0;
-var wrongAnswer = 0;
+var correctScore = 0;
+var wrongScore = 0;
 var timerRunning = false;
-var gameBody = $('.gameBody');
-var answerLog = $('.answerLog');
-var timer = $('#timer');
-var intervalId;
-var myQuestions = [ {
-    question: 'What is the last name of the brothers in Supernatural?',
-    answers: {
-    a: 'Winchester',
-    b: 'Robinson',
-    c: 'Powers',
-    d: 'Hunter',
-},
-correct1: a,
-},
-{
-    question: 'What was the name of Dexters wife in Dexter on Showtime?',
-    answers: {
-    after: 'Vanessa',
-    b: 'Courtney',
-    c: 'Rita',
-    d: 'Lily',
-},
-correct: c,
-},
-{
-    question: 'What is the name of the town in the Simpsons?',
-    answers: {
-    a: 'Ashburn',
-    b: 'Wakefield',
-    c: 'Rockville',
-    d: 'Springfield',
-},
-correct: d,
-},
-{
-    question: 'What is the name of the Governors daughter in The Walking Dead?',
-    answers: {
-    a: 'Laura',
-    b: 'Penny',
-    c: 'Hillary',
-    c: 'Maggie',
-},
-correct: b,
-},
-]
+var timerNumber = 90;
+var intervalID;
+
 //Have JS/Jquery start after document is loaded//
 $(document).ready(function () {
-
-    //Hide Main Game Area
-    $('.gameBody').hide();
-    $('.answerLog').hide();
-    //Reset Function//
-    function reset() {
-        timer = 90;
-        $('#timer').text(timer);
-    }
-    //Create timer functions//
-    function timerGoing() {
-        timer = 90;
-        $('#timer').text(timer);
-        if (!timerRunning) {
-            intervalId = setInterval(timer.count, 1000);
-            timerRunning = true;
-            timer--;
-        };
-
+    function resetGame() {
+        $('#startButton').show();
+        $('.gameBody').hide();
+        $('.answerLog').hide();
+        correctScore = 0;
+        wrongScore = 0;
     };
-    //Create a on click function to show the Main Game area when Start is clicked and starts the timer//
+    resetGame();
+    
     $('#startButton').on('click', function () {
         $('#startButton').hide();
         $('.gameBody').show();
-        timerGoing();
     });
 
-})
-//Create a condtion that recognizes when the user answers a question correctly 
-//Add it to the Correct Answer counter
+    //Create timer functions//
 
-//Create a condition that recognizes an incorrect answer
-//Add it to the Wrong Answer counter
+    function submitAnswers() {
+        var questions = 5;
+        var q1 = $('input[name=q1]:checked').val();
+        var q2 = $('input[name=q2]:checked').val();
+        var q3 = $('input[name=q3]:checked').val();
+        var q4 = $('input[name=q4]:checked').val();
+        var q5 = $('input[name=q5]:checked').val();
+        var answers = ['c', 'b', 'a', 'a', 'c'];
+
+        for (i = 1; i <= questions.length; i++) {
+            if (eval('q'+ 1) == answers[0 + 1]) {
+                correctScore++;
+                $('#correctAnswers').html('Correct Answers:' + correctScore.toString());
+            } else {
+                wrongScore++;
+                $('#wrongAnswers').html('Wrong Answers:' + wrongScore.toString());
+            };
+        };
+    };
+    submitAnswers();
+
+    $("#doneButton").on('click', function () {
+        $('.gameBody').hide();
+        $('.answerLog').show();
+    });
+
+    $('#reStart').on('click', function () {
+        resetGame();
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+})// final end
+
 
